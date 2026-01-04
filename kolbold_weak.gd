3413638a4ca355d25a1a8b7ad026c038e14f6f15
Kevin Li <jingxl18@uci.edu>
2026-01-04 18:21:33 +0800

    step2 enemy AI

--- kolbold_weak.gd
+++ kolbold_weak.gd
@@ -1,15 +1,14 @@
 extends CharacterBody2D
 
 
-var speed = 40.0
+@export var speed = 20.0
 var direction = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	movement()
 
 func movement():
-	direction = Vector2(Input.get_axis("left", "right"),
-	Input.get_axis("up", "down"))
+	direction = %player.global_position - global_position
 	direction = direction.normalized()
 	velocity = direction * speed
 	move_and_slide()
