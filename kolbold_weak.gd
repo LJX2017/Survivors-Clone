11e70d7f049ee2880e9eb02d59916457986d3b4e
Kevin Li <jingxl18@uci.edu>
2026-01-04 22:24:33 +0800

    step3 enemy animation

--- kolbold_weak.gd
+++ kolbold_weak.gd
@@ -3,12 +3,21 @@ extends CharacterBody2D
 
 @export var speed = 20.0
 var direction = Vector2.ZERO
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
+	update_animation()
 
 func movement(delta: float):
 	direction = %player.global_position - global_position
 	direction = direction.normalized()
 	velocity = direction * speed
+	animated_sprite.play("walk")
 	move_and_collide(velocity * delta)
+
+func update_animation():
+	if direction.x > 0:
+		animated_sprite.flip_h = true
+	elif direction.x < 0:
+		animated_sprite.flip_h = false
