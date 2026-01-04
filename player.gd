65081fce13f217a1d0af6ab7d039223c69fb06b8
Kevin Li <jingxl18@uci.edu>
2026-01-04 22:12:17 +0800

    step2 player animation

--- player.gd
+++ player.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 
 var speed = 40.0
 var direction = Vector2.ZERO
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
+	update_animation()
 
 func movement(delta: float):
 	direction = Vector2(Input.get_axis("left", "right"),
@@ -13,3 +15,13 @@ func movement(delta: float):
 	direction = direction.normalized()
 	velocity = direction * speed
 	move_and_collide(velocity * delta)
+	
+func update_animation():
+	if direction != Vector2.ZERO:
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.stop()
+	if direction.x > 0:
+		animated_sprite.flip_h = true
+	elif direction.x < 0:
+		animated_sprite.flip_h = false
