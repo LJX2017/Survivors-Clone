ea79f152e7b227118af55e0fc4a90ee545b420f2
Kevin Li <jingxl18@uci.edu>
2026-01-05 09:09:07 +0800

    step2 hit box

---
+++ Utils/hit_box.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var damage = 2
+@onready var collision = $CollisionShape2D
+@onready var disable_timer = $DisableTimer
+
+func temp_disable():
+	collision.set_deferred("disabled", true)
+	disable_timer.start()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_disable_timer_timeout() -> void:
+	collision.set_deferred("disabled", false)
--- Utils/hurt_box.gd
+++ Utils/hurt_box.gd
@@ -3,8 +3,8 @@ extends Area2D
 @export_enum("Cooldown", "DisableHitbox") var HurtBoxType: int
 
 @onready var collision = $CollisionShape2D
-
-signal(hurt, damage)
+@onready var disable_timer = $DisableTimer
+signal hurt(damage)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("damage"):
@@ -12,7 +12,13 @@ func _on_area_entered(area: Area2D) -> void:
 			match HurtBoxType:
 				0:
 					collision.set_deferred("disabled", true)
+					disable_timer.start()
 				1:
 					if area.has_method("temp_disable"):
 						area.temp_disable()
 			emit_signal("hurt", area.damage)
+
+
+
+func _on_disable_timer_timeout() -> void:
+	collision.set_deferred("disabled", false)
