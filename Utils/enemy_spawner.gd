b87d595577e1b03e731575a643bc0335071a666f
Kevin Li <jingxl18@uci.edu>
2026-01-10 08:40:50 +0800

    create spawner

--- Utils/enemy_spawner.gd
+++ Utils/enemy_spawner.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var spawns: Array[Resource] = []
+@export var spawns: Array[Spawn_info] = []
 
 @onready var player = %player
 
@@ -8,4 +8,17 @@ extends Node2D
 
 
 func _on_timer_timeout() -> void:
-	pass # Create a new 
+	time += 1
+	var enemy_spawns = spawns
+	for i in enemy_spawns:
+		if i.time_start <= time and time <= i.time_end:
+			if i.spawn_delay_counter < i.enemy_spawn_delay:
+				i.spawn_delay_counter += 1
+			else:
+				i.spawn_delay_counter = 0
+				var new_enemy = i.enemy
+				for j in range(i.enemy_num):
+					var enemy_spawn = new_enemy.instantiate()
+					enemy_spawn.player = player
+					enemy_spawn.global_position = Vector2.ZERO
+					add_child(enemy_spawn)
--- kolbold_weak.gd
+++ kolbold_weak.gd
@@ -4,13 +4,14 @@ extends CharacterBody2D
 @export var speed = 20.0
 var direction = Vector2.ZERO
 @onready var animated_sprite = $AnimatedSprite2D
+var player: Node2D
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
 	update_animation()
 
 func movement(delta: float):
-	direction = %player.global_position - global_position
+	direction = player.global_position - global_position
 	direction = direction.normalized()
 	velocity = direction * speed
 	animated_sprite.play("walk")
