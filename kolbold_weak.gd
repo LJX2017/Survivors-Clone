5d0d264a80e79439089bda6cff15b995aad8edbf
Kevin Li <jingxl18@uci.edu>
2026-01-12 01:35:28 +0800

    step1 enemy explosion

--- kolbold_weak.gd
+++ kolbold_weak.gd
@@ -5,7 +5,12 @@ extends CharacterBody2D
 @export var speed = 20.0
 var direction = Vector2.ZERO
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+@onready var hitbox = $hit_box
 var player: Node2D
+
+func _ready() -> void:
+	animated_sprite.play("walk")
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
@@ -15,7 +20,6 @@ func movement(delta: float):
 	direction = player.global_position - global_position
 	direction = direction.normalized()
 	velocity = direction * speed
-	animated_sprite.play("walk")
 	move_and_collide(velocity * delta)
 
 func update_animation():
@@ -28,4 +32,11 @@ func update_animation():
 func _on_hurt_box_hurt(damage: Variant) -> void:
 	hp -= damage
 	if hp <= 0:
+		animated_sprite.play("explode")
+		collision_layer = 0
+		collision_mask = 0
+		set_physics_process(false)
+		collision.set_deferred("disabled", true)
+		hitbox.set_deferred("disable_mode", true)
+		await animated_sprite.animation_finished
 		queue_free()
