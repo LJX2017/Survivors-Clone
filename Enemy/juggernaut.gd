947f8ecd62ad2f84d9d387f12c58271840490fb6
Kevin Li <jingxl18@uci.edu>
2026-02-07 23:59:09 +0800

    step1 add nodes

---
+++ Enemy/juggernaut.gd
@@ -0,0 +1,6 @@
+extends CharacterBody2D
+var player: Node2D
+
+
+func _on_hurt_box_hurt(damage: float, knockback_direction: Vector2, knockback_amount: float) -> void:
+	pass # Replace with function body.
